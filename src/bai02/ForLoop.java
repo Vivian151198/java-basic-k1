b4219714b667f946eb111a93d81ddfbd1f5625d6
van vo <vanminho296@gmail.com>
2021-07-17 22:14:44 +0700

    change value

--- src/bai02/ForLoop.java
+++ src/bai02/ForLoop.java
@@ -6,7 +6,7 @@ public class ForLoop {
 
         // n! = 1*2*....*(n-1)*n
 //        int initValue = 1;
-        int giaithua = 1;
+        int giaithua = 2;
 
         for (int initValue = 1; initValue < 6; initValue++) {
             giaithua = giaithua * initValue;
